95abce538c83f3d16622f495c10997599e0ec932
Zack Gomez <zack.gomez@gmail.com>
2017-03-06 01:41:28 -0800

    Add README, serve /api/data and public files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Imperial Assault Campaign Tracker
+
+## Features
+
+- Tracks cards in the side mission and agenda decks
+- Tracks rebel heros experience, credits, allies, upgrades and equipment
+- Tracks imperial player's experience, influence, purchased agendas, agenda discard, and upgrades
+- Tracks sold items
+- Tracks current mission area
+- Tracks thread level and item decks during Rebel upgrade step
--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get('/api/data', function (req, res) {
+  var data = {
+    name: 'Test Campaign',
+    campaign_type: 'Return to Hoth',
+    completed_missions: ['hoth_intro', 'side_mission_1'],
+    imperial_player: {
+      name: 'Alice',
+    },
+    rebel_players: [
+    {
+      name: 'Bob',
+      hero: 'diala',
+    },
+    {
+      name: 'Charlie',
+      hero: 'loku',
+    },
+    ],
+  };
+  res.send(data);
 });
+
+app.use(express.static('public'));
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
