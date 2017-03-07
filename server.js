c448ce4d31b2bb4cdf8da4a89fd3946807113bfd
Zack Gomez <zack.gomez@gmail.com>
2017-03-06 21:52:51 -0800

    Add a bit more data and convert api/data to camelCase

--- server.js
+++ server.js
@@ -4,19 +4,48 @@ var app = express();
 app.get('/api/data', function (req, res) {
   var data = {
     name: 'Test Campaign',
-    campaign_type: 'Return to Hoth',
-    completed_missions: ['hoth_intro', 'side_mission_1'],
-    imperial_player: {
+    campaignType: 'Return to Hoth',
+    completedMissions: ['hoth_intro', 'side_mission_1'],
+    imperialPlayer: {
       name: 'Alice',
+      classDeck: 'super_minions',
+      totalXP: 4,
+      currentXP: 1,
+      influence: 2,
+      upgrades: [
+        'super_minions_1',
+        'super_minions_3',
+      ],
     },
-    rebel_players: [
+    rebelTotalCredits: 1750,
+    rebelCurrentCredits: 400,
+    rebelTotalXP: 5,
+    rebelPlayers: [
     {
       name: 'Bob',
       hero: 'diala',
+      currentXP: 0,
+      upgrades: [
+        'diala_1',
+        'diala_2',
+      ],
+      equipment: [
+        'lightsaber',
+        'headband',
+      ],
     },
     {
       name: 'Charlie',
       hero: 'loku',
+      currentXP: 2,
+      upgrades: [
+        'loku_1',
+        'loku_3',
+      ],
+      equipment: [
+        'sniper_rifle',
+        'invisibility_cloak',
+      ],
     },
     ],
   };
